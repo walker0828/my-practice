05c5bcb44bf7559bbbce30419690ce646945a554
yachao.ni <yachao.ni@ipinyou.com>
2021-12-23 13:49:27 +0800

    路漫漫

---
+++ hundreds/src/main/java/com/example/one/ExampleOne.java
@@ -0,0 +1,13 @@
+package com.example.one;
+
+public class ExampleOne {
+    class BSTree {
+        int value;
+        BSTree left;
+        BSTree right;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
